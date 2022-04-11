2a23b5ff62b8c2ba321aa7ec689726439059738f
luckygy618 <luckygy618@gmail.com>
2022-04-11 13:42:36 -0400

    a4 finished

--- app/src/main/java/com/guoyucao/jokeguru/MyApp.java
+++ app/src/main/java/com/guoyucao/jokeguru/MyApp.java
@@ -5,54 +5,29 @@ import android.app.Application;
 import com.guoyucao.jokeguru.api.DataService;
 import com.guoyucao.jokeguru.api.JsonService;
 import com.guoyucao.jokeguru.database.DatabaseManager;
-import com.guoyucao.jokeguru.database.JokeEntity;
 
 import java.util.ArrayList;
 
 public class MyApp extends Application {
 
-    ArrayList<Joke> jokes;
-    ArrayList<Joke> savedJokes;
-    Joke test = new Joke("This is a test joke 666.", "Programming", "001");
-
-    DatabaseManager dbManager = new DatabaseManager();
-    JokeEntity mainJokeEntity = new JokeEntity();
     private final DataService dataService = new DataService();
     private final JsonService jsonService = new JsonService();
+    ArrayList<Joke> jokes;
+    ArrayList<Joke> savedJokes;
+    DatabaseManager dbManager = new DatabaseManager();
 
     public MyApp() {
-        jokes = new ArrayList<Joke>();
-        savedJokes = new ArrayList<Joke>();
+        jokes = new ArrayList<>();
+        savedJokes = new ArrayList<>();
     }
 
     public DatabaseManager getDbManager() {
         return dbManager;
     }
 
-    public void setDbManager(DatabaseManager dbManager) {
-        this.dbManager = dbManager;
-    }
-
-    public JokeEntity getMainJokeEntity() {
-        return mainJokeEntity;
-    }
-
-    public void setMainJokeEntity(JokeEntity mainJokeEntity) {
-        this.mainJokeEntity = mainJokeEntity;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-    }
-
-    public ArrayList<Joke> getSavedJokes() {
-        return savedJokes;
-    }
-
-    public void setSavedJokes(ArrayList<Joke> savedJokes) {
-        this.savedJokes = savedJokes;
     }
 
     public ArrayList<Joke> getJokes() {
@@ -67,10 +42,7 @@ public class MyApp extends Application {
         return jsonService;
     }
 
-
     public DataService getDataService() {
         return dataService;
     }
-
-
 }
